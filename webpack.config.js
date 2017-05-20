465207fcf27ec8b2139d18028d9c14fd1a316cd8
McGregor88 <ruslan.igorevich@list.ru>
2017-05-20 22:50:25 +0300

    pug and pug loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,17 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack app'
+            template: PATHS.src + '/index.pug'
         })
-    ]
+    ], module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            }
+        ]
+    }
 };
